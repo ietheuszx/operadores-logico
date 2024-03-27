b36361ee42cf55833bb5ea2ad244c2d49df34548
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:05:52 -0400

    terminado questão 13

---
+++ questao13.js
@@ -0,0 +1,8 @@
+/*
+13. O que significa a expressão !(5 !== 5)
+                                !(false)
+                                true
+*/
+
+let resultado = !(5 !== 5)
+console.log(resultado)
