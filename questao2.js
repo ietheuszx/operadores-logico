ab53ca1126a3def8a8a1f3906dbfb3e133a87b60
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:03:44 -0400

    terminado questão 2

---
+++ questao2.js
@@ -0,0 +1,9 @@
+/*
+2. O que retorna a expressão (true || false) && (false || true)?
+           
+                                  true       &&       true
+                                            true
+*/
+
+let resultado = (true || false) && (false || true)
+console.log(resultado)
