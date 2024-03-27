5e5bdf52b2af8a8f904ff57d2724fd11997d895c
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:08:52 -0400

    terminado questão 30

---
+++ questao30.js
@@ -0,0 +1,11 @@
+/*
+Explique como você pode usar operadores lógicos para verificar se um 
+número é negativo e maior que -10 em JavaScript
+*/
+
+let numero = -7;
+if (numero < 0 && numero > -10 ) {
+    console.log("Pertence a sentença")
+} else {
+    console.log("Não pertence a sentença")
+}
