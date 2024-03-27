32604c9d4d7c9a68bebbf9f088dfc2edddb3dee0
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:07:00 -0400

    terminado questão 22

---
+++ questao22.js
@@ -0,0 +1,7 @@
+/*O que retorna a expressão false || (true && false)
+                 false || false 
+                 false 
+*/
+
+let resultado = false || (true && false)
+console.log(resultado)
