fe980dd3fb24b4e953c7172f9f53e3c9d1550b73
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:05:20 -0400

    terminado questão 10

---
+++ questao10.js
@@ -0,0 +1,8 @@
+/*
+10. Qual é o resultado de !true || false && true?
+                          false || false
+                               false
+*/
+
+let resultado = !true || false && true
+console.log(resultado)
