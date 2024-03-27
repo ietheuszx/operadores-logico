3b5a60cd73fc7b0fa423cc97eeb16802b18711a4
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:08:00 -0400

    terminado questão 27

---
+++ questao27.js
@@ -0,0 +1,7 @@
+/*Qual é o resultado de (5 === 5) || !(4 !== 4)
+                           true   ||    true 
+                                 true 
+*/
+
+let resultado = (5 === 5) || !(4 !== 4)
+console.log(resultado)
