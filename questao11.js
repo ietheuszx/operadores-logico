086a45351da38ea2c93671bf520ad7e6be657962
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:05:28 -0400

    terminado questão 11

---
+++ questao11.js
@@ -0,0 +1,6 @@
+/*
+11. Explique a diferença entre os operadores && e || em JavaScript. 
+
+&&: E exemplo: A&&B (verdadeiro se existir A e B)
+||: Ou exemplo: A||B (verdadeiro se existir A ou B)
+*/
