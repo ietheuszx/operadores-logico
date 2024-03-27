f80cf6abe8880ef2964a3b7d81d6422858a1de27
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:06:06 -0400

    terminado questão 15

---
+++ questao15.js
@@ -0,0 +1,6 @@
+/*
+15. Explique como a ordem de avaliação dos operadores lógicos afeta o 
+resultado de uma expressão em JavaScript. 
+
+como vimos na operação anterior, a ordem dos fatores podem alterar o resultado.
+*/
