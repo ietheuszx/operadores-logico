09bd8dfc536888672eedf5376b0559be90d18061
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:06:31 -0400

    terminado questão 18

---
+++ questao18.js
@@ -0,0 +1,8 @@
+/*
+O que retorna a expressão (true || false) && !(false && true)
+                               true       &&      !false    
+                                         true      
+*/
+
+let resultado =  (true || false) && !(false && true)
+console.log(resultado)
