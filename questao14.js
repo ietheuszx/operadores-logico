f2bf78831bc086a284a4507dfcc593078a8562a1
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:06:00 -0400

    terminado questão 14

---
+++ questao14.js
@@ -0,0 +1,9 @@
+/*
+14. Qual é o resultado de (10 > 5) && !(3 < 1)?
+                          true && !false
+                          true && true
+                          true
+*/
+
+let resultado = (10 > 5) && !(3 < 1)
+console.log(resultado)
