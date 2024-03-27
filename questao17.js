909ddda66621dd4db13a0e44fc78542ffb2f4191
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:06:17 -0400

    terminado questão 17

---
+++ questao17.js
@@ -0,0 +1,8 @@
+/*Explique o que significa a expressão !(10 <= 5)
+                                       !false 
+                                        true     
+                              
+*/
+
+let resultado = !(10 <= 5)
+console.log(resultado)
