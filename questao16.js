b6f4683225a97939321f97c4ed157524fa3ecdc6
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:06:11 -0400

    terminado questão 16

---
+++ questao16.js
@@ -0,0 +1,8 @@
+/*
+Qual é o resultado de (true && true) || (false && false)
+                           true      ||      false 
+                                    true
+*/
+
+let resultado = (true && true) || (false && false)
+console.log(resultado)
