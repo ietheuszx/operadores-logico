200dc737ba3f9b57c1a31d2ea5cde37aac140ded
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:04:55 -0400

    terminado questão 9

---
+++ questao9.js
@@ -0,0 +1,8 @@
+/*
+9. O que retorna a expressão false && (true || false)?
+                             false &&      true
+                                   false
+*/
+
+let resultado = false && (true || false)
+console.log(resultado)
