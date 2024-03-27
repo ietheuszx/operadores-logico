7d9c17090767537ca8a0c81c91e7ffd1ce251324
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:06:54 -0400

    terminado questão 21

---
+++ questao21.js
@@ -0,0 +1,11 @@
+/*
+Explique como você pode usar operadores lógicos para verificar se uma 
+pessoa tem idade entre 18 e 65 anos em JavaScrip
+*/
+
+let idade = 26;
+if (idade >= 18 && idade <= 65) {
+    console.log("Está contido")
+} else {
+    console.log("Não está contido");
+}
