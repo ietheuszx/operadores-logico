18524a75c9157bbfc935942321a830359bd9b480
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:07:53 -0400

    terminado questão 26

---
+++ questao26.js
@@ -0,0 +1,5 @@
+/*
+Explique como os parênteses afetam o resultado de uma expressão com 
+operadores lógicos em JavaScript.
+*/
+// Os parenteses afetam porque dizem que eles devem ser feitos antes das outras sentenças
