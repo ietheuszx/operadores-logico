3f881b6dafb4dbb5bfd9ac93d13f5f0a2c3d7f27
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:07:08 -0400

    terminado questão 23

---
+++ questao23.js
@@ -0,0 +1,7 @@
+/*Qual é o resultado de !(7 > 3) || (6 <= 2)
+                           false || false
+                               false        
+*/
+
+let resultado = !(7 > 3) || (6 <= 2)
+console.log(resultado)
