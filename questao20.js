17cfe6f45ac351d1e22eb3928576bd126b309ae4
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:06:46 -0400

    terminado questão 20

---
+++ questao20.js
@@ -0,0 +1,8 @@
+/*
+Qual é o resultado de (5 >= 5) && (10 === 10)
+                        true   &&    true 
+                              true 
+*/
+
+let resultado = (5 >= 5) && (10 === 10);
+console.log(resultado)
