3f637c1b16f51ddf73a54aba60df70ca1b44099c
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:04:08 -0400

    terminado questão 5

---
+++ questao5.js
@@ -0,0 +1,10 @@
+/*
+5. Qual é o resultado de (8 !== 8) || (3 <= 3)? 
+
+                           false   ||   true
+                                  true
+                                   
+*/
+
+let resultado = (8 !== 8) || (3 <= 3)
+console.log(resultado)
