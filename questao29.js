ce5c39a9f403b524733d14e380ec19bef87906a9
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:08:13 -0400

    terminado questão 28

---
+++ questao29.js
@@ -0,0 +1,7 @@
+/*O que retorna a expressão (true && false) || (true && true)
+                                 false      ||      true 
+                                           true
+*/
+
+let resultado = (true && false) || (true && true)
+console.log(resultado)
