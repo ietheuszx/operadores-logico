cc19498b9761d550f08d72a5d74db7061d5314d8
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:03:33 -0400

    terminado questão 1

---
+++ questao1.js
@@ -0,0 +1,9 @@
+/*
+1. O resultado de (10 > 5) && (7 < 9) é verdadeiro ou falso?
+
+                    true   &&   true 
+                          true
+*/
+
+let resultado = (10 > 5) && (7 < 9)
+console.log(resultado)
