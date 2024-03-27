7836e07caa3f929b8cde8d350c12fc5b2b9367bb
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:03:52 -0400

    terminado questão 3

---
+++ questao3.js
@@ -0,0 +1,10 @@
+/*
+3. Qual é o resultado de !(5 === 3) && (4 >= 4)?
+
+                           !false   &&   true 
+                            true    &&   true
+                                   
+*/
+
+let resultado = !(5 === 3) && (4 >= 4)
+console.log(resultado)
