542b1c7bcf9a2028ce9063d1b7084fdbe9ec4913
ietheuszx <fernandomaranss68@gmail.com>
2024-03-27 15:07:24 -0400

    terminado questão 25

---
+++ questao25.js
@@ -0,0 +1,7 @@
+/*O que significa a expressão true && !(false || true)
+                              true && !true 
+                                   false 
+*/
+
+let resultado = true && !(false || true)
+console.log(resultado)
